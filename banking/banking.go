506b769da903de89dbb7989cccb5228c02e9ee5d
welcomehyunseo <92869725+welcomehyunseo@users.noreply.github.com>
2022-03-08 21:50:11 +0900

    my bank account system

--- banking/banking.go
+++ banking/banking.go
@@ -1,6 +1,47 @@
 package banking
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Account struct {
-	Owner   string
-	Balance int
+	owner   string
+	balance int
+}
+
+var errNoMoney = errors.New("cat't withdraw")
+
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+func (a *Account) SetBalance(amount int) {
+	a.balance = amount
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+
+	a.balance -= amount
+	return nil
+}
+
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint("\n", a.owner, "'s account.\nHas:", a.balance, "\n")
+}
+
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a *Account) Balance() int {
+	return a.balance
 }
--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/welcomehyunseo/bank-account/banking"
 )
 
 func main() {
-	account := banking.Account{Owner: "hyunseo", Balance: 1000}
-	fmt.Println(account)
+	account := banking.NewAccount("hyunseo")
+	account.SetBalance(10)
+	fmt.Println(account.String())
+
+	// error handling in golang
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(account.String())
 }
